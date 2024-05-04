399e3a7c6750ba7a7d3613aa85b00b7f6946d38b
AkiGR <hanauchi.aki@gmail.com>
2024-05-04 22:31:53 +0900

    fix ABC352 B

--- ABC352/b.cpp
+++ ABC352/b.cpp
@@ -10,12 +10,8 @@ int main() {
     int sIndex = 0; // Sのインデックスを先頭から開始
     int tIndex = 0; // Tのインデックスを先頭から開始
 
-    vector<int> AnsIndex;
-
     while (sIndex < S.length() && tIndex < T.length()) {
         if (S[sIndex] == T[tIndex]) {
-            // SとTの文字が一致する場合、正しい入力文字として扱う
-            AnsIndex.push_back(tIndex + 1);
             cout << tIndex + 1 << " "; // 一致した位置を出力
             sIndex++;
         }
