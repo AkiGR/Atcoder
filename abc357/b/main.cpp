aaf2281a0e101d17cd82614f0ad4ad916dd31b39
AkiGR <hanauchi.aki@gmail.com>
2024-06-09 00:49:16 +0900

    join ABC357 A,B AC

---
+++ abc357/b/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int upperCount = 0;
+    int lowerCount = 0;
+    for (char c : s) {
+        if (isupper(c)) {
+            upperCount++;
+        } else if (islower(c)) {
+            lowerCount++;
+        }
+    }
+    if (upperCount > lowerCount) {
+        transform(s.begin(), s.end(), s.begin(), ::toupper);
+    } else {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+    }
+    cout << s << endl;
+    return 0;
+}
