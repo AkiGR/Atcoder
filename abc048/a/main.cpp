1704f1bd24ebeeb65c52a4c61598cfaa705fff0f
AkiGR <hanauchi.aki@gmail.com>
2024-07-15 14:25:23 +0900

    ABC048 A,B AC

---
+++ abc048/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// define
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  string a,s,c;
+  cin >> a >> s >> c;
+  cout << 'A' + s.substr(0,1) + 'C' << endl;
+  return 0;
+}
