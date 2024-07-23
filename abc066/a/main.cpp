fbab2a83986d346c518f2d2912d1ffad1cac34cb
AkiGR <hanauchi.aki@gmail.com>
2024-07-23 10:36:11 +0900

    ABC066 A,B AC

---
+++ abc066/a/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// define
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int a,b,c;
+  cin >> a >> b >> c;
+  if (a > b) swap(a,b);
+  if (a > c) swap(a,c);
+  if (b > c) swap(b,c);
+
+  cout << a+b << endl;
+  return 0;
+}
