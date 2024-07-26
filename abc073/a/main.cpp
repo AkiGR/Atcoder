e411074adecc3683649314897051d966014c9205
AkiGR <hanauchi.aki@gmail.com>
2024-07-26 12:03:33 +0900

    ABC073 A,B AC

---
+++ abc073/a/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// define
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  string s;
+  cin >> s;
+
+  if (s[0] == '9' || s[1] == '9') {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
