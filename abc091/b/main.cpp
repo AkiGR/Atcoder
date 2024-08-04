54f69a5fdcaebd9765e04bd779fb7feb55ae7152
AkiGR <hanauchi.aki@gmail.com>
2024-08-04 11:42:17 +0900

    ABC091 A,B AC

---
+++ abc091/b/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// define
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main(){
+    int N, M;
+    cin >> N;
+    vector<string> B(N);
+    for(int i = 0; i < N; i++) cin >> B[i];
+    cin >> M;
+    vector<string> R(M);
+    for(int i = 0; i < M; i++) cin >> R[i];
+
+    map<string, int> blue;
+    for(int i = 0; i < N; i++){
+        blue[B[i]]++;
+    }
+    map<string, int> red;
+    for(int i = 0; i < M; i++){
+        red[R[i]]++;
+    }
+
+    int max = 0;
+    for(auto b : blue){
+        if(b.second - red[b.first] > max){
+            max = b.second - red[b.first];
+        }
+    }
+    cout << max << endl;
+}
